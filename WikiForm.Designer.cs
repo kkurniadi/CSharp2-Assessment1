1afd35085ec00e38d39b4fba665aac5ac7fd2cc0
30045816 <30045816@TB06EBF2D30AF>
2022-02-15 09:34:51 +0800

    Add project files.

---
+++ WikiForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace AssessmentOne
+{
+    partial class WikiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxStructure = new System.Windows.Forms.TextBox();
+            this.textBoxDefinition = new System.Windows.Forms.TextBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(18, 23);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(99, 23);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 1;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(180, 23);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(18, 68);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.PlaceholderText = "Name";
+            this.textBoxName.Size = new System.Drawing.Size(237, 23);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(18, 106);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.PlaceholderText = "Category";
+            this.textBoxCategory.Size = new System.Drawing.Size(237, 23);
+            this.textBoxCategory.TabIndex = 4;
+            // 
+            // textBoxStructure
+            // 
+            this.textBoxStructure.Location = new System.Drawing.Point(18, 146);
+            this.textBoxStructure.Name = "textBoxStructure";
+            this.textBoxStructure.PlaceholderText = "Structure";
+            this.textBoxStructure.Size = new System.Drawing.Size(237, 23);
+            this.textBoxStructure.TabIndex = 5;
+            // 
+            // textBoxDefinition
+            // 
+            this.textBoxDefinition.Location = new System.Drawing.Point(18, 184);
+            this.textBoxDefinition.Multiline = true;
+            this.textBoxDefinition.Name = "textBoxDefinition";
+            this.textBoxDefinition.PlaceholderText = "Definition";
+            this.textBoxDefinition.Size = new System.Drawing.Size(237, 132);
+            this.textBoxDefinition.TabIndex = 6;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(302, 24);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(190, 23);
+            this.textBoxSearch.TabIndex = 7;
+            // 
+            // listBoxDisplay
+            // 
+            this.listBoxDisplay.FormattingEnabled = true;
+            this.listBoxDisplay.ItemHeight = 15;
+            this.listBoxDisplay.Location = new System.Drawing.Point(286, 68);
+            this.listBoxDisplay.Name = "listBoxDisplay";
+            this.listBoxDisplay.Size = new System.Drawing.Size(287, 214);
+            this.listBoxDisplay.TabIndex = 8;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(286, 293);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 9;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(367, 293);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(498, 24);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 11;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // WikiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 342);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.listBoxDisplay);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxDefinition);
+            this.Controls.Add(this.textBoxStructure);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "WikiForm";
+            this.Text = "Data Structures Wiki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonAdd;
+        private Button buttonEdit;
+        private Button buttonDelete;
+        private TextBox textBoxName;
+        private TextBox textBoxCategory;
+        private TextBox textBoxStructure;
+        private TextBox textBoxDefinition;
+        private TextBox textBoxSearch;
+        private ListBox listBoxDisplay;
+        private Button buttonOpen;
+        private Button buttonSave;
+        private Button buttonSearch;
+    }
+}
---
+++ WikiForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Kirsten Kurniadi, ID: 30045816
+namespace AssessmentOne
+{
+    public partial class WikiForm : Form
+    {
+        public WikiForm()
+        {
+            InitializeComponent();
+        }
+        static int entries = 12;
+        static int attributes = 4;
+        int pointer = 0;
+        string[,] wikiData = new string[entries, attributes];
+
+        private void DisplayData()
+        {
+            listBoxDisplay.Items.Clear();
+            for (int x = 0; x < entries; x++)
+            {
+                string term = wikiData[x, 0];
+                listBoxDisplay.Items.Add(term);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddTerm()
+        {
+            if (pointer < entries)
+            {
+                try
+                {
+                    wikiData[pointer, 0] = textBoxName.Text;
+                    wikiData[pointer, 1] = textBoxCategory.Text;
+                    wikiData[pointer, 2] = textBoxStructure.Text;
+                    wikiData[pointer, 3] = textBoxDefinition.Text;
+                    pointer++;
+                }
+                catch
+                {
+                    MessageBox.Show("Nobody wants any more data structures to define");
+                }
+            }
+        }
+    }
+}
