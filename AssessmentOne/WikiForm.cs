3838b92340420ee3ae2776c2fc6b1dc0ddfd6977
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-03-07 17:26:26 +0800

    Update WikiForm.cs
    Added display method and important variables

--- AssessmentOne/WikiForm.cs
+++ AssessmentOne/WikiForm.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// Kirsten Kurniadi, ID: 30045816
 namespace AssessmentOne
 {
     public partial class WikiForm : Form
@@ -15,6 +15,21 @@ namespace AssessmentOne
         public WikiForm()
         {
             InitializeComponent();
+        }
+        static int entries = 12;
+        static int attributes = 4;
+        string defaultFileName = "definitions.dat";
+        int pointer = 0;
+        string[,] wikiData = new string[entries, attributes];
+
+        private void DisplayNameCat()
+        {
+            ListBoxDisplay.Items.Clear();
+            for (int x = 0; x < pointer; x++)
+            {
+                string term = wikiData[x, 0] + "\t" + wikiData[x, 1];
+                ListBoxDisplay.Items.Add(term);
+            }
         }
     }
 }
