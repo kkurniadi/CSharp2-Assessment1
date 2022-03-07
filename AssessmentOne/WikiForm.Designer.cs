99e94ecb89fc48381a2a79dd4978c91f4d7b3136
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-03-07 17:13:07 +0800

    Create new project files
    Create project files using the correct version of the .NET Framework

---
+++ AssessmentOne/WikiForm.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace AssessmentOne
+{
+    partial class WikiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonDelete = new System.Windows.Forms.Button();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
+            this.TextBoxCategory = new System.Windows.Forms.TextBox();
+            this.TextBoxStructure = new System.Windows.Forms.TextBox();
+            this.TextBoxDefinition = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBoxSearch = new System.Windows.Forms.TextBox();
+            this.ListBoxDisplay = new System.Windows.Forms.ListBox();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(12, 12);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAdd.TabIndex = 0;
+            this.ButtonAdd.Text = "Add";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            // 
+            // ButtonEdit
+            // 
+            this.ButtonEdit.Location = new System.Drawing.Point(93, 12);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEdit.TabIndex = 1;
+            this.ButtonEdit.Text = "Edit";
+            this.ButtonEdit.UseVisualStyleBackColor = true;
+            // 
+            // ButtonDelete
+            // 
+            this.ButtonDelete.Location = new System.Drawing.Point(174, 12);
+            this.ButtonDelete.Name = "ButtonDelete";
+            this.ButtonDelete.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDelete.TabIndex = 2;
+            this.ButtonDelete.Text = "Delete";
+            this.ButtonDelete.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxName
+            // 
+            this.TextBoxName.Location = new System.Drawing.Point(12, 66);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.Size = new System.Drawing.Size(237, 20);
+            this.TextBoxName.TabIndex = 3;
+            // 
+            // TextBoxCategory
+            // 
+            this.TextBoxCategory.Location = new System.Drawing.Point(12, 105);
+            this.TextBoxCategory.Name = "TextBoxCategory";
+            this.TextBoxCategory.Size = new System.Drawing.Size(237, 20);
+            this.TextBoxCategory.TabIndex = 4;
+            // 
+            // TextBoxStructure
+            // 
+            this.TextBoxStructure.Location = new System.Drawing.Point(12, 144);
+            this.TextBoxStructure.Name = "TextBoxStructure";
+            this.TextBoxStructure.Size = new System.Drawing.Size(237, 20);
+            this.TextBoxStructure.TabIndex = 5;
+            // 
+            // TextBoxDefinition
+            // 
+            this.TextBoxDefinition.Location = new System.Drawing.Point(12, 183);
+            this.TextBoxDefinition.Multiline = true;
+            this.TextBoxDefinition.Name = "TextBoxDefinition";
+            this.TextBoxDefinition.Size = new System.Drawing.Size(237, 112);
+            this.TextBoxDefinition.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Structure";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Definition";
+            // 
+            // TextBoxSearch
+            // 
+            this.TextBoxSearch.Location = new System.Drawing.Point(280, 14);
+            this.TextBoxSearch.Name = "TextBoxSearch";
+            this.TextBoxSearch.Size = new System.Drawing.Size(155, 20);
+            this.TextBoxSearch.TabIndex = 11;
+            // 
+            // ListBoxDisplay
+            // 
+            this.ListBoxDisplay.FormattingEnabled = true;
+            this.ListBoxDisplay.Location = new System.Drawing.Point(280, 67);
+            this.ListBoxDisplay.Name = "ListBoxDisplay";
+            this.ListBoxDisplay.Size = new System.Drawing.Size(236, 199);
+            this.ListBoxDisplay.TabIndex = 12;
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Location = new System.Drawing.Point(441, 12);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSearch.TabIndex = 13;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(277, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "List of Terms/Definitions";
+            // 
+            // ButtonOpen
+            // 
+            this.ButtonOpen.Location = new System.Drawing.Point(280, 272);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOpen.TabIndex = 15;
+            this.ButtonOpen.Text = "Open";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Location = new System.Drawing.Point(361, 272);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSave.TabIndex = 16;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            // 
+            // WikiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 309);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonOpen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ListBoxDisplay);
+            this.Controls.Add(this.TextBoxSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxDefinition);
+            this.Controls.Add(this.TextBoxStructure);
+            this.Controls.Add(this.TextBoxCategory);
+            this.Controls.Add(this.TextBoxName);
+            this.Controls.Add(this.ButtonDelete);
+            this.Controls.Add(this.ButtonEdit);
+            this.Controls.Add(this.ButtonAdd);
+            this.Name = "WikiForm";
+            this.Text = "Wiki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Button ButtonEdit;
+        private System.Windows.Forms.Button ButtonDelete;
+        private System.Windows.Forms.TextBox TextBoxName;
+        private System.Windows.Forms.TextBox TextBoxCategory;
+        private System.Windows.Forms.TextBox TextBoxStructure;
+        private System.Windows.Forms.TextBox TextBoxDefinition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextBoxSearch;
+        private System.Windows.Forms.ListBox ListBoxDisplay;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button ButtonOpen;
+        private System.Windows.Forms.Button ButtonSave;
+    }
+}
+
